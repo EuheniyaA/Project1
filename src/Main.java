497b5e98ec5cb66296e7f02144c16b9c8d70be11
Evgeniya <bolbat_evgenia@mail.ru>
2021-10-09 00:23:51 +0300

    change info in report

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-public  class  Main {
-    public  static  void  main ( String [] args ) {
-        int currentBalance =  2_000_000_000 ;
-        int transferAmount =  500_000_000 ;
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
         int finalCurrentBalance = currentBalance + transferAmount;
         System.out.println(finalCurrentBalance);
     }
